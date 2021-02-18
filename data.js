721d8e53d1fc0b73261b45d0742f0e926cbc600e
2unbini <75126613+2unbini@users.noreply.github.com>
2021-02-18 16:57:54 +0900

    coffee_test 1.0

---
+++ data.js
@@ -0,0 +1,190 @@
+var q = {
+    1 : {
+        type: "EI",
+        title: "1번 문제",
+        A: "A형 답변",
+        B: "B형 답변"
+    },
+    2 : {
+        type: "EI",
+        title: "2번 문제",
+        A: "A형 답변",
+        B: "B형 답변"
+    },
+    3 : {
+        type: "EI",
+        title: "3번 문제",
+        A: "A형 답변",
+        B: "B형 답변"
+    },
+    4 : {
+        type: "SN",
+        title: "4번 문제",
+        A: "A형 답변",
+        B: "B형 답변"
+    },
+    5 : {
+        type: "SN",
+        title: "5번 문제",
+        A: "A형 답변",
+        B: "B형 답변"
+    },
+    6 : {
+        type: "SN",
+        title: "6번 문제",
+        A: "A형 답변",
+        B: "B형 답변"
+    },
+    7 : {
+        type: "TF",
+        title: "7번 문제",
+        A: "A형 답변",
+        B: "B형 답변"
+    },
+    8 : {
+        type: "TF",
+        title: "8번 문제",
+        A: "A형 답변",
+        B: "B형 답변"
+    },
+    9 : {
+        type: "TF",
+        title: "9번 문제",
+        A: "A형 답변",
+        B: "B형 답변"
+    },
+    10 : {
+        type: "JP",
+        title: "10번 문제",
+        A: "A형 답변",
+        B: "B형 답변"
+    },
+    11 : {
+        type: "JP",
+        title: "11번 문제",
+        A: "A형 답변",
+        B: "B형 답변"
+    },
+    12 : {
+        type: "JP",
+        title: "12번 문제",
+        A: "A형 답변",
+        B: "B형 답변"
+    }
+}
+
+
+var result = {
+    "ESTJ": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "ESTP": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "ESFJ": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "ESFP": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "ENFJ": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "ENTJ": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "ENFP": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "ENTP": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "ISTJ": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "ISTP": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "ISFJ": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "ISFP": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "INFJ": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "INTJ": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "INFP": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            }, 
+    "INTP": {
+            "drink": "차가운 아메리카노",
+            "explain": "차가운 아메리카노를 좋아하는 당신은 차가운 아메리카노를 좋아하는군요!",
+            "dessert": "치즈케이크",
+            "img-drk": "iamericano.jpg",
+            "img-dess": "cheesecake.jpg"
+            },
+};
